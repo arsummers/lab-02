a8c108fbe109ae21dd46a9b83073f031b60e9910
Aliya Summers <aliyasummers1@gmail.com>
2019-03-13 09:29:57 -0700

    made Critter constructor

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # lab-02
+
+Number and name of feature: 1, Display images
+
+Estimate of time needed to complete: 1 hour 
+
+Start time: _9:23
+Finish time: _____
+
+Actual time needed to complete: _____
--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 'use strict';
+//pull the JSON file
+//get data,
+//put data in array,
+//.then
+
+//constructor function for horned critters
+let Critter = function(image_url, title, description, keyword, horns){
+  this.image_url = image_url;
+  this.title = title;
+  this.description = description;
+  this.keyword = keyword;
+  this.horns = horns;
+}
+
